e58e73447747c0bb8a7e2998ba5221b370942ebd
antonio-muniz <ahnmhenrique@gmail.com>
2020-05-25 14:40:41 -0300

    Create alph application

---
+++ cmd/alph/components.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"github.com/sarulabs/di"
+)
+
+func initializeComponents() (di.Container, error) {
+	builder, err := di.NewBuilder()
+	if err != nil {
+		return nil, errors.Wrap(err, "error building components")
+	}
+	container := builder.Build()
+	return container, nil
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/sarulabs/di v2.0.0+incompatible
 	github.com/stretchr/testify v1.5.1
 )
